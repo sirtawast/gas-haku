e43a96034184270f1f596477a5cff26013b611bd
Sampo Tawast <sampo.tawast@gmail.com>
2020-01-04 13:57:49 +0200

    Use s3 to store results to

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const xray = require('x-ray')
+const S3 = require('aws-sdk/clients/s3');
+const s3 = new S3();
+
+
 const x = xray({  filters: {
     jpegFull: function(value) {
       return typeof value === 'string' && (value.indexOf('-q.jpg') > 0 || value.indexOf('-n.jpg') > 0) ? value.replace(/-[a-z]\.jpg/ig, '.jpg') : value;
@@ -7,14 +11,12 @@ const x = xray({  filters: {
       return typeof value === 'string'? value.replace('Hinta: ', '') : value;
     },
     formatId: function(value) {
-      return typeof value === 'string'? value.split("/ilmoitus/")[1] : value;
+      return typeof value === 'string'? parseInt(value.split("/ilmoitus/")[1]) : value;
     }
   }
 })
 
-
-
-x('https://muusikoiden.net/tori/haku.php?keyword=mikseri&city=091&type=sell', 'table+script+table table[cellpadding="2"]', [
+const stream = x('https://muusikoiden.net/tori/haku.php?keyword=mikseri&city=091&type=sell', 'table+script+table table[cellpadding="2"]', [
   {
     id:    '.bg2 .tori_title a@href | formatId',
     link:  '.bg2 .tori_title a@href',
@@ -27,4 +29,11 @@ x('https://muusikoiden.net/tori/haku.php?keyword=mikseri&city=091&type=sell', 't
 ])
   .paginate('table p td[align="right"] a[href*="/tori/haku.php?"]:last-of-type@href')
   .limit(2)
-  .write('results.json')
+  .stream()
+  // .write('results.json')
+
+var params = {ACL: 'private', Bucket: 'my-summer-bucket', Key: 'results.json', Body: stream};
+
+s3.upload(params, function(err, data) {
+  console.log(err, data);
+});
